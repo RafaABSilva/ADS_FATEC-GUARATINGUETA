1c35746ab27e107fda206c823a8814b219409ed7
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2017-03-14 23:14:43 -0300

    Projeto 1 - 1 Semáforo
    Atualização da prototipação e do Código Fonte.

--- Projeto 1 - 1 Semáforo/Codigo-Fonte_Projeto1/Codigo-Fonte_Projeto1.ino
+++ Projeto 1 - 1 Semáforo/Codigo-Fonte_Projeto1/Codigo-Fonte_Projeto1.ino
@@ -1,23 +1,23 @@
 void setup() {
   //Definindo as Portas e os parâmetros das mesmas
-  pinMode(13, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(13, OUTPUT); // Vermelho
+  pinMode(7, OUTPUT); // Amarelo
+  pinMode(4, OUTPUT); // Verde
 }
 
 void loop() {
   //Led Vermelho
-  digitalWrite(13, HIGH);
+  digitalWrite(13, HIGH); // Liga Vermelho
   delay(5000);
-  digitalWrite(13, LOW);
-
-  //Led Amarelo
-  digitalWrite(4, HIGH);
-  delay(5000);
-  digitalWrite(4, LOW);
+  digitalWrite(13, LOW); // Desliga Vermelho
   
-  //Led Verde
-  digitalWrite(7, HIGH);
+  //Led Amarelo
+  digitalWrite(7, HIGH); // Liga Amarelo
   delay(2000);
-  digitalWrite(7, LOW);
-  }
+  digitalWrite(7, LOW); // Desliga Amarelo
+
+  //Led Verde
+  digitalWrite(4, HIGH); // Liga Verde
+  delay(5000);
+  digitalWrite(4, LOW); // Desliga Verde
+}
