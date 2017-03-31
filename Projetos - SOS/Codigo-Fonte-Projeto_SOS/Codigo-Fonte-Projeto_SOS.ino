a198111d42c566b182ff516e9fd6da4fecb85875
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2017-03-31 10:23:33 -0300

    Projetos - SOS
    Atualização do Código Fonte.

---
+++ Projetos - SOS/Codigo-Fonte-Projeto_SOS/Codigo-Fonte-Projeto_SOS.ino
@@ -0,0 +1,101 @@
+//Declaração das Variáveis
+int buzzer =10;
+int led =13;
+
+void setup() {
+    pinMode(buzzer,OUTPUT); //Definindo o tipo da porta
+    pinMode(led, OUTPUT);  //Definindo o tipo da porta
+}
+
+void loop() {  
+
+  // 3 Longos
+      //1º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(500);
+    
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(500);
+
+      //2º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(500);
+    
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(500);
+      
+      //3º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(500);
+    
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(500);
+ 
+  // 3 Curtos
+      //1º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(250);
+      
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(250);
+  
+      //2º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(250);
+      
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(250);
+
+      //3º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(250);
+      
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(250);
+
+  // 3 Longos
+      //1º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(500);
+    
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(500);
+
+      //2º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(500);
+    
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(500);
+      
+      //3º
+      digitalWrite(buzzer,HIGH); // Liga Buzzer
+      digitalWrite(led, HIGH); //Liga LED
+      delay(500);
+    
+      digitalWrite(buzzer,LOW); //Desliga Buzzer
+      digitalWrite(led, LOW); //Desliga LED
+      delay(500);
+  
+  
+  
+  
+
+  
+}
