468e42085364d2c5d46310be915ed3115ff07473
JoaoPauloCunha <jcpfcunha@gmail.com>
2017-06-19 22:29:51 -0300

    Organização dos Relatórios
    Metade da Organização dos relatórios concluída.

--- Projeto 4.1 - Blink (Buzzer)/Codigo-Fonte_Projeto4.1/Codigo-Fonte_Projeto4.1.ino
+++ Projeto 4.1 - Blink (Buzzer)/Codigo-Fonte_Projeto4.1/Codigo-Fonte_Projeto4.1.ino
@@ -1,12 +1,12 @@
-int led = 13;
+int buzzer = 13;
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
+  digitalWrite(buzzer, HIGH);
   delay(1000);
-  digitalWrite(led, LOW);
+  digitalWrite(buzzer, LOW);
   delay(1000);
 }
